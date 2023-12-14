e82801456144a6f0ae892ec729605964e57e8e38
cathautzen <cathautzen@gmail.com>
2023-12-14 17:00:40 +0100

    Rettelse af profiler

--- profiler.php
+++ profiler.php
@@ -3,13 +3,11 @@
 
 <body>
 
-<div class="cirkel">
-</div>
 
 <div class="container">
     <div class="row">
         <div class="col text-center mt-5">
-            <img class="justify-content-center" style="width: 100px; " src="images/logo_HeartlyHello.png" alt="HeartlyHello Logo" >
+            <img class="justify-content-center" style="width: 100px;" src="images/logo_HeartlyHello.png" alt="HeartlyHello Logo" >
         </div>
     </div>
 </div>
@@ -18,10 +16,10 @@
 <div class="container">
     <div class="row">
         <div class="col mt-1">
-            <h4 class="fst-italic text-white">Diversitet</h4>
-            <h4 class="fst-italic text-white">Venskaber</h4>
-            <h4 class="fst-italic text-white">Dybe forbindelser</h4>
-            <h4 class="fw-bold text-white">- Start din søgen nu</h4>
+            <h4 class="fst-italic ">Diversitet</h4>
+            <h4 class="fst-italic ">Venskaber</h4>
+            <h4 class="fst-italic ">Dybe forbindelser</h4>
+            <h4 class="fw-bold ">- Start din søgen nu</h4>
         </div>
     </div>
 </div>
@@ -34,6 +32,59 @@
         </div>
     </div>
 </div>
+
+<div class="row mt-5">
+    <div class="col-6">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Navn</h5>
+                <p class="card-text">Beskrivelse</p>
+                <a href="#" class="btn btn-orange">Besøg Profil</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-6">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Navn</h5>
+                <p class="card-text">Beskrivelse</p>
+                <a href="#" class="btn btn-orange">Besøg Profil</a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row mt-5">
+    <div class="col-6">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Navn</h5>
+                <p class="card-text">Beskrivelse</p>
+                <a href="#" class="btn btn-orange">Besøg Profil</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-6">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Navn</h5>
+                <p class="card-text">Beskrivelse</p>
+                <a href="#" class="btn btn-orange">Besøg Profil</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<footer class="container" style="border-top: 1px solid #ccc">
+    <div class="row">
+        <div class="col d-flex justify-content-evenly align-items-end">
+                <a href="#"><i class="fas fa-home fa-2xl" style="color: #C6C6C6;" alt="Hjem"></i></a>
+                <a href="#"><i class="fas fa-user fa-2xl" style="color: #C6C6C6;" alt="Din profil"></i></a>
+                <img style="width: 40px;" src="images/logo_HeartlyHello.png" alt="Profiler">
+                <a href="#"><i class="fas fa-message fa-2xl" style="color: #C6C6C6;" alt="Beskeder"></i></a>
+                <a href="#"><i class="fas fa-bars fa-2xl" style="color: #C6C6C6;" alt="Menu"></i></a>
+        </div>
+    </div>
+</footer>
 
 
 <script src="https://kit.fontawesome.com/645a3a1c42.js" crossorigin="anonymous"></script> <!--Fontawesome ikoner-->
